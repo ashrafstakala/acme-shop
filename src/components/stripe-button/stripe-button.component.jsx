276e5fc626699d505404ff16547f2bdd930a6d12
ashrafstakala <ashrafstakala@gmail.com>
2022-03-12 12:51:05 +0200

    stripe checkout

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51KZinmDzorLwFCDie07hmO8DERlzR17LUVKpmQmp9QCCbmqy5CGcfWXeWtJjLUS4G0upEWNvtIGtjFr3QkwhQjz500GArGIOtO';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Succesfull');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Acme Shop Inc."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
