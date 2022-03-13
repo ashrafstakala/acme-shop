34d4840b67be4fd8c22ceba2da41c1f49a132509
ashrafstakala <ashrafstakala@gmail.com>
2022-03-13 22:02:22 +0200

    saga refactor - signout

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -22,3 +22,16 @@ export const emailSignInStart = (emailAndPassword) => ({
 export const checkUserSession = () => ({
   type: UserActionTypes.CHECK_USER_SESSION,
 });
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START,
+});
+
+export const signOutSucess = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS,
+});
+
+export const signOutFailure = (error) => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
+  payload: error,
+});
